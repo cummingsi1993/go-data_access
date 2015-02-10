972c4480be2ffdad96c54d66b9ee566ea679f6d4
Isaac Cummings <Cummingsi1993@gmail.com>
2015-02-10 14:52:05 -0500

    finishing touches

--- couchbaseDAL.go
+++ couchbaseDAL.go
@@ -1,17 +1,34 @@
-package couchbase
+package DataAccess
 
 import (
 	"github.com/couchbaselabs/go-couchbase"
+	"fmt"
 )
 
-func Get(dbLocation string, key string, value interface{}) (err error) {
-	c, err := couchbase.Connect("http://localhost:8091/")
+type couchbaseConfig struct {
+	DBLocation string
+	PoolName string
+	BucketName string
+}
+
+func GetCouchbaseDAL(dbLocation string, poolName string, bucketName string) (IDataAccess) { 
+	//return couchbaseConfig{dbLocation, poolName, bucketName}
+	config := new(couchbaseConfig)
+	config.DBLocation = dbLocation
+	config.PoolName = poolName
+	config.BucketName = bucketName
+	return config
+}
+
+func (config couchbaseConfig) Get(key string, value interface{}) (err error) {
+	fmt.Println(config.DBLocation)
+	c, err := couchbase.Connect(config.DBLocation)
 	if (err != nil) { return }
 
-	pool, err := c.GetPool("default")
+	pool, err := c.GetPool(config.PoolName)
 	if (err != nil) { return }
 
-	bucket, err := pool.GetBucket("SecretThing")
+	bucket, err := pool.GetBucket(config.BucketName)
 	if (err != nil) { return }
 	defer bucket.Close()
 
@@ -21,32 +38,30 @@ func Get(dbLocation string, key string, value interface{}) (err error) {
 	return
 }
 
-func Remove(dbLocation string, key string) (err error) {
-	c, err := couchbase.Connect(dbLocation)
+func (config couchbaseConfig) Remove(key string) (err error) {
+	c, err := couchbase.Connect(config.DBLocation)
 	if (err != nil) { return }
 
-	pool, err := c.GetPool("default")
+	pool, err := c.GetPool(config.PoolName)
 	if (err != nil) { return }
 
-	bucket, err := pool.GetBucket("SecretThing")
+	bucket, err := pool.GetBucket(config.BucketName)
 	if (err != nil) { return }
 	defer bucket.Close()
 
 	err = bucket.Delete(key)
 
-	bucket.Close()
-
 	return
 }
 
-func Set(dbLocation string, key string, value interface{}) (err error) { 
-	c, err := couchbase.Connect(dbLocation)
+func (config couchbaseConfig) Set(key string, value interface{}) (err error) { //key string, 
+	c, err := couchbase.Connect(config.DBLocation)
 	if (err != nil) { return }
 
-	pool, err := c.GetPool("default")
+	pool, err := c.GetPool(config.PoolName)
 	if (err != nil) { return }
 
-	bucket, err := pool.GetBucket("SecretThing")
+	bucket, err := pool.GetBucket(config.BucketName)
 	if (err != nil) { return }
 	defer bucket.Close()
 
